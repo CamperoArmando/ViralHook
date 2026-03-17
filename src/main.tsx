e48d79f96a2eb0006a55828f390ede066f5ef086
LucaXGit <luisgabrielborgesvazquez@gmail.com>
2026-03-16 20:41:17 -0600

    main

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,10 @@ import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 
+// Punto de entrada de la aplicación
+// Inicializa React en el div con id 'root' de index.html
 ReactDOM.createRoot(document.getElementById('root')!).render(
+  // Envolvemos la App en BrowserRouter para habilitar routing
   <BrowserRouter>
     <App />
   </BrowserRouter>
