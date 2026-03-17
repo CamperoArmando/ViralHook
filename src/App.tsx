4381efcfaccf9da4e0dc42ee048fee86d1e11be9
LucaXGit <luisgabrielborgesvazquez@gmail.com>
2026-03-16 20:40:04 -0600

    app

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import GeneratorPage from "./pages/GeneratorPage.tsx";
 
+// Componente principal de la app que define las rutas
 function App() {
   return (
     <Routes>
+      {/* Ruta principal: pantalla de login */}
       <Route path="/" element={<Login />} />
+      {/* Ruta de registro de usuarios */}
       <Route path="/register" element={<Register />} />
+      {/* Ruta de la herramienta de generación de hooks */}
       <Route path="/app" element={<GeneratorPage />} />
     </Routes>
   );
